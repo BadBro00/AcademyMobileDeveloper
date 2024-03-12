86632399d7b75a261120b030efe898884c90d01d
Eugenio Militerno <82472181+BadBro00@users.noreply.github.com>
2024-03-12 17:41:05 +0100

    Update Test.cs

--- Giorno2/Test.cs
+++ Giorno2/Test.cs
@@ -1,6 +1,4 @@
-ES 1:
-
-/*
+/*ES 1:
             Scrivere un programma che esegue il calcolo dell'area di un rettangolo
             leggendo la dimensione dei lati da Console. Utilizzare il tipo double
                  
@@ -49,14 +47,13 @@ Esercizio 3
                 Ogni ingrediente può essere inserito più di una volta:
                 se accade, si dovrebbe aggiungere nuovamente le calorie all'importo totale.
                 Al termine, stampa le calorie totali.
-
-
 */
+using System;
 public class Giorno2{
     public static void Main(){
         //Esercizio 1
         //Dichiaro le variabili
-        double h,w,area;
+        double h,w;
         Console.WriteLine("Inserisci l'altezza del rettangolo: ");
         h = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine("Inserisci la larghezza del rettangolo: ");
@@ -64,43 +61,150 @@ public class Giorno2{
         //Stampa dell'area
         Console.WriteLine($"L'area del rettangolo è: {h*w}");
         //Esercizio 2
-        //Uso le pair per collegare costo e capienza (capacità)
-        pair[] sale = new pair[3];
-        //Inizializzo le coppie
-        sale[0] = new pair(2500,50);
-        sale[1] = new pair(5000,100);
-        sale[2] = new pair(7500,120);
-        //Array di pacchetti
-        pair[] pacchetti = new pair[3];
-        pacchetti[0] = new pair(500,5);
-        pacchetti[1] = new pair(750,10);
-        pacchetti[2] = new pair(1000,15);
-        int s,p;
-        //Selezione da parte dell'utente
-        Console.WriteLine("Inserisci la sala desiderata (0: piccola, 1: terrazza, 2: grande): ");
-        s = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Inserisci il pacchetto desiderato (0: normale, 1: gold, 2: platinum): ");
-        p = Convert.ToInt32(Console.ReadLine());
-        //Stampa del prezzo totale
-        Console.WriteLine($"Il prezzo totale è: {sale[s].first+pacchetti[p].first-(sale[s].first+pacchetti[p].first*0.01*pacchetti[p].second)}");
+        //Inizializzo i costi, sconti e capacità
+        int costo1 = 2500;
+        int costo2 = 5000;
+        int costo3 = 7500;
+        //----------
+        int sconto1 = 500;
+        int sconto2 = 750;
+        int sconto3 = 1000;
+        //----------
+        int cap1 = 50;
+        int cap2 = 100;
+        int cap3 = 120;
+        //-----------
+        int pers,sala,pack;
+        double sconto,prezzo;
+        //Scelta sala
+        Console.WriteLine("Scegli la sala:\n1)50ps - EUR 2500\n2)100ps - EUR 5000\n3)120ps - EUR 7500: ");
+        sala = Convert.ToInt32(Console.ReadLine());
+        switch (sala){
+            //Scelta la sala 1
+            case 1:
+                prezzo = costo1;
+                //Scelta del pacchetto
+                Console.WriteLine("Scegli il pacchetto:\n1)5% - EUR 500\n2)10% - EUR 750\n3)15% - EUR 1000: ");
+                pack = Convert.ToInt32(Console.ReadLine());
+                switch(pack){
+                    //Scelto il pacchetto 1
+                    case 1:
+                        sconto = 5;
+                        prezzo+=sconto1;
+                        break;
+                    //Scelto il pacchetto 2
+                    case 2:
+                        sconto = 10;
+                        prezzo+=sconto2;
+                        break;
+                    //Scelto il pacchetto 3
+                    case 3:
+                        sconto = 15;
+                        prezzo+=sconto3;
+                        break;
+                    //Qualunque altra scelta inserita
+                    default:
+                        Console.WriteLine("Pacchetto non valido");
+                        return;
+                }
+                break;
+            case 2:
+                prezzo = costo2;
+                Console.WriteLine("Scegli il pacchetto:\n1)5% - EUR 500\n2)10% - EUR 750\n3)15% - EUR 1000: ");
+                pack = Convert.ToInt32(Console.ReadLine());
+                switch(pack){
+                    case 1:
+                        sconto = 5;
+                        prezzo+=sconto1;
+                        break;
+                    case 2:
+                        sconto = 10;
+                        prezzo+=sconto2;
+                        break;
+                    case 3:
+                        sconto = 15;
+                        prezzo+=sconto3;
+                        break;
+                    default:
+                        Console.WriteLine("Pacchetto non valido");
+                        return;
+                }
+                break;
+            case 3:
+                prezzo = costo3;
+                Console.WriteLine("Scegli il pacchetto:\n1)5% - EUR 500\n2)10% - EUR 750\n3)15% - EUR 1000: ");
+                pack = Convert.ToInt32(Console.ReadLine());
+                switch(pack){
+                    case 1:
+                        sconto = 5;
+                        prezzo+=sconto1;
+                        break;
+                    case 2:
+                        sconto = 10;
+                        prezzo+=sconto2;
+                        break;
+                    case 3:
+                        sconto = 15;
+                        prezzo+=sconto3;
+                        break;
+                    default:
+                        Console.WriteLine("Pacchetto non valido");
+                        return;
+                }
+                break;
+            //Qualunque altra scelta inserita
+            default:
+                Console.WriteLine("Sala non valida");
+                return;
+        }
+        //Controllo sul numero di persone (giusto per)
+        Console.WriteLine("Inserisci il numero di persone: ");
+        pers = Convert.ToInt32(Console.ReadLine());
+        if(pers<cap1||pers>cap3){
+            Console.WriteLine("Numero di persone non valido");
+            return;
+        }
+        //Calcolo del prezzo
+        double tot=prezzo-(prezzo*(sconto*0.01));
+        Console.WriteLine($"Il prezzo totale è: {tot}");
         //Esercizio 3
         //Array di calorie in base ai vari ingredienti
-        int[] calorie = new int[4];
-        calorie[0] = 500;
-        calorie[1] = 300;
-        calorie[2] = 40;
-        calorie[3] = 50;
+        int tot_cal = 0;
+        int cal_ing1 = 500;
+        int cal_ing2 = 300;
+        int cal_ing3 = 40;
+        int cal_ing4 = 50;
         //Array di quantità per gestire gli ingredienti
-        int[] quantita = new int[4];
-        int tot = 0;
+        int cal_tot = 0;
         //Ciclo for per iterare su ciascun ingrediente, e quindi anche le capacità
         for(int i=0;i<4;i++){
-            Console.WriteLine($"Inserisci la quantità di ingrediente {i+1}: ");
-            quantita[i] = Convert.ToInt32(Console.ReadLine());
-            //Aumento le calorie totali in base alle quantità di ciascun ingrediente
-            tot += quantita[i]*calorie[i];
+            int times = 0;
+            Console.WriteLine($"Inserisci quante volte inserire l'ingrediente: {i+1}: ");
+            times = Convert.ToInt32(Console.ReadLine());
+            for(int j=0;j<times;j++){
+                int qt = 0;
+                switch(i){
+                    case 0:
+                        Console.WriteLine($"Inserisci la quantità di calorie per l'ingrediente {i+1}: ");
+                        qt = Convert.ToInt32(Console.ReadLine());
+                        cal_tot+=cal_ing1*qt;
+                        break;
+                    case 1:
+                        qt = Convert.ToInt32(Console.ReadLine());
+                        cal_tot+=cal_ing2*qt;
+                        break;
+                    case 2:
+                        qt = Convert.ToInt32(Console.ReadLine());
+                        cal_tot+=cal_ing3*qt;
+                        break;
+                    case 3:
+                        qt = Convert.ToInt32(Console.ReadLine());
+                        cal_tot+=cal_ing4*qt;
+                        break;
+                }
+            }
         }
         //Stampa del risultato
-        Console.WriteLine($"Le calorie totali sono: {tot}");
+        Console.WriteLine($"Le calorie totali sono: {cal_tot}");
     }
 }
